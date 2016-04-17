bf6f7c09f7f9bb23f0fb2714dc092cd63a39f140
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-17 22:26:33 +0900

    sample consensus

--- src/PCLSampleConsensus.jl
+++ src/PCLSampleConsensus.jl
@@ -1,5 +1,67 @@
 module PCLSampleConsensus
 
-# package code goes here
+using LibPCL
+using PCLCommon
+using Cxx
+
+const libpcl_sample_consensus = LibPCL.find_library_e("libpcl_sample_consensus")
+try
+    Libdl.dlopen(libpcl_sample_consensus, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
+
+cxx"""
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+"""
+
+for enumname in [
+    :SACMODEL_PLANE,
+    :SACMODEL_LINE,
+    :SACMODEL_CIRCLE2D,
+    :SACMODEL_CIRCLE3D,
+    :SACMODEL_SPHERE,
+    :SACMODEL_CYLINDER,
+    :SACMODEL_CONE,
+    :SACMODEL_TORUS,
+    :SACMODEL_PARALLEL_LINE,
+    :SACMODEL_PERPENDICULAR_PLANE,
+    :SACMODEL_PARALLEL_LINES,
+    :SACMODEL_NORMAL_PLANE,
+    :SACMODEL_NORMAL_SPHERE,
+    :SACMODEL_REGISTRATION,
+    :SACMODEL_REGISTRATION_2D,
+    :SACMODEL_PARALLEL_PLANE,
+    :SACMODEL_NORMAL_PARALLEL_PLANE,
+    :SACMODEL_STICK
+    ]
+    # build icxx expr
+    ex = Expr(:macrocall, symbol("@icxx_str"), string("pcl::", enumname, ";"))
+    @eval begin
+        enumtyp = $ex
+        isa(enumtyp, Cxx.CppEnum)
+        global const $enumname = enumtyp
+        export $enumname
+    end
+end
+
+for intname in [
+    :SAC_RANSAC,
+    :SAC_LMEDS,
+    :SAC_MSAC,
+    :SAC_RRANSAC,
+    :SAC_RMSAC,
+    :SAC_MLESAC,
+    :SAC_PROSAC,
+    ]
+    ex = Expr(:macrocall, symbol("@icxx_str"), string("pcl::", intname, ";"))
+    @eval begin
+        global const $intname = $ex
+        @assert isa($intname, Cint)
+        export $intname
+    end
+end
 
 end # module
